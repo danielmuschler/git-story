dc78b63dbcbc3d2c2146f6d0c680b8ba7c9532ed
danielmuschler <dmusch4@gmail.com>
2018-10-03 18:46:58 -0500

    and added in line 1

--- story.rb
+++ story.rb
@@ -1,2 +1,2 @@
-this is a story all about how my life got flipped turned upside down. Now Id like to take a minute just sit right here tell you how I became the prince of a town called Bel-Air >> story.rb
+and and and and and this is a story all about how my life got flipped turned upside down. Now Id like to take a minute just sit right here tell you how I became the prince of a town called Bel-Air >> story.rb
 ~/Desktop/actualize/git-story$ echo this is a story all about how my life got flipped turned upside down. Now Id like to take a minute just sit right here tell you how I became the prince of a town called Bel-Air
